7d0a9a30922b58c28fa778af6ace1b0866770b70
asadanik <s.engineer63@gmail.com>
2021-04-14 21:46:43 +0600

    Selection Sorting Algorithms Program is done

--- DSA/Basic/SelectionSort.py
+++ DSA/Basic/SelectionSort.py
@@ -1,11 +1,11 @@
 ### Selection sort in python..
-#The selection sort algorithm sorts an array by repeatedly finding the minimum element(considering ascending order) from unsorted part and putting it at the beginning.
+# The selection sort algorithm sorts an array by repeatedly finding the minimum element(considering ascending order) from unsorted part and putting it at the beginning.
 # The algorithm maintains two subarrays in a given array.
 
 # 1) The subarray which is already sorted.
 # 2) Remaining subarray which is unsorted.
 
-#In every iteration of selection sort,
+# In every iteration of selection sort,
 # the minimum element(considering ascending order) from the unsorted subarray is picked and moved to the sorted subarray.
 
 
